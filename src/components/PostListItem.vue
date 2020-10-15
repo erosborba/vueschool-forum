6f43944c1d407cd017ca425a7501271468016012
eros.borba <borba.eros@gmail.com>
2020-10-14 22:13:30 -0300

    Add date features

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -16,14 +16,18 @@
       </div>
     </div>
 
-    <div class="post-date text-faded">
-      {{ post.publishedAt }}
+    <div
+      class="post-date text-faded"
+      :title="post.publishedAt | humanFriendlyDate"
+    >
+      {{ post.publishedAt | diffForHumans }}
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import moment from 'moment'
 export default {
   props: {
     post: {
@@ -37,6 +41,14 @@ export default {
     },
     userPostsCount() {
       return Object.keys(this.user.posts).length
+    }
+  },
+  filters: {
+    humanFriendlyDate(date) {
+      return moment.locale('pt'), moment.unix(date).format('lll')
+    },
+    diffForHumans(date) {
+      return moment.unix(date).fromNow()
     }
   }
 }
